8cd647e97215dbd30f47bab1f588aed92e269035
Ali Al-Muzaini <far7es751@gmail.com>
2026-07-14 01:50:55 +0300

    Implement contact API with SES

--- frontend/script.js
+++ frontend/script.js
@@ -31,3 +31,44 @@ async function getVisitorCount() {
 }
 
 getVisitorCount();
+
+
+
+const form = document.getElementById("contact-form");
+
+form.addEventListener("submit", async (event) => {
+
+    event.preventDefault();
+
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const message = document.getElementById("message").value;
+
+    const response = await fetch(
+        "https://a9jxr6f5z0.execute-api.us-east-1.amazonaws.com/contact",
+        {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                name,
+                email,
+                message
+            })
+        }
+    );
+
+    const data = await response.json();
+
+const status = document.getElementById("contact-status");
+
+status.textContent = data.message;
+
+if (response.ok) {
+    status.style.color = "#3fb950";
+} else {
+    status.style.color = "#ff6b6b";
+}
+
+});
